f32a2e269ee7a55a1907416f7d1d9dd941d8c567
arnaud-eb <arnaud.depierreux@easebest.com>
2020-11-09 10:49:00 +0100

    Updates

---
+++ precache-manifest.79adcf80d8eb27cee0d5e2abcbc5c552.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d727deeec0ca4588cb3a63879dc2a61",
+    "url": "/zombie-react/index.html"
+  },
+  {
+    "revision": "9533dcdcce9e7a93f4bf",
+    "url": "/zombie-react/static/css/2.af317cf5.chunk.css"
+  },
+  {
+    "revision": "0af5aac30ecae3168b1e",
+    "url": "/zombie-react/static/css/main.848331c8.chunk.css"
+  },
+  {
+    "revision": "9533dcdcce9e7a93f4bf",
+    "url": "/zombie-react/static/js/2.86ad6cdf.chunk.js"
+  },
+  {
+    "revision": "0af5aac30ecae3168b1e",
+    "url": "/zombie-react/static/js/main.4a1d54da.chunk.js"
+  },
+  {
+    "revision": "9154cf90cdfda32be068",
+    "url": "/zombie-react/static/js/runtime-main.68b83ccc.js"
+  }
+]);
